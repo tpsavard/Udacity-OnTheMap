cbd8108a39172b4a55b58d65125eb6f337d02673
Timothy Savard <tpsavard@gmail.com>
2016-10-04 21:06:37 -0700

    Added action flow for login attempt.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -29,11 +29,11 @@ class LoginViewController: UIViewController {
     
     @IBAction func login(sender: UIButton) {
         print("login IBAction called")
+        login()
     }
     
     @IBAction func textFieldUpdated(_ sender: AnyObject) {
         print("textFieldUpdated called")
-        
         enableLogin()
     }
     
@@ -42,6 +42,44 @@ class LoginViewController: UIViewController {
     
     func enableLogin() {
         loginButton.isEnabled = !(usernameField.text!.isEmpty || passwordField.text!.isEmpty)
+    }
+    
+    func login() {
+        let username: String = usernameField.text!
+        let password: String = passwordField.text!
+        
+        NSLog("Attemping login with \(username)")
+        
+        // Doing a log in...
+        let loginResult = NetworkRequests.LoginResults.failedForNetworkingError
+        
+        // Handle the login outcome
+        switch loginResult {
+        case NetworkRequests.LoginResults.success:
+            NSLog("Login succeeded")
+            performSegue(withIdentifier: "LoggedIn", sender: nil)
+        case NetworkRequests.LoginResults.failedForCredentials:
+            NSLog("Login failed for invalid credentials")
+            showLoginFailureAlert(message: NSLocalizedString("LoginCredentialFailure", comment: "Credentials failure text"))
+        case NetworkRequests.LoginResults.failedForNetworkingError:
+            NSLog("Login failed for networking error")
+            showLoginFailureAlert(message: NSLocalizedString("LoginNetworkFailure", comment: "Network failure text"))
+        }
+    }
+    
+    func showLoginFailureAlert(message: String) {
+        let alertController: UIAlertController = UIAlertController(
+            title: NSLocalizedString("LoginFailureTitle", comment: "Login failed alert title"),
+            message: message,
+            preferredStyle:UIAlertControllerStyle.alert)
+        let alertAction: UIAlertAction = UIAlertAction(
+            title: NSLocalizedString("OK", comment: "Not good-OK, not bad-OK, just OK-OK"),
+            style: UIAlertActionStyle.default,
+            handler: nil)
+        
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
+
     }
 
 }
