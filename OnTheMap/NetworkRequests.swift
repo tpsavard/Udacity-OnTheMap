82dcf02f50509dace5f546264463d99c07ec589c
Timothy Savard <timsav@amazon.com>
2016-10-20 13:06:57 -0700

    Added log out networking code.

--- OnTheMap/NetworkRequests.swift
+++ OnTheMap/NetworkRequests.swift
@@ -28,7 +28,7 @@ class NetworkRequests {
         request.httpBody = "{\"udacity\": {\"username\": \"\(username)\", \"password\": \"\(password)\"}}".data(using: String.Encoding.utf8)
         
         // Send the request to Udacity
-        NSLog("Attemping login with \(username)")
+        NSLog("Attemping logi n with \(username)")
         let session = URLSession.shared
         let task = session.dataTask(with: request) { data, response, error in
             // Catch any communication errors
@@ -66,7 +66,42 @@ class NetworkRequests {
     }
     
     func logOut(completionHandler: @escaping (Results) -> ()) {
+        // Setup the basic request arguments
+        var request = URLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+        request.httpMethod = "DELETE"
         
+        // Find the XSRF cookie and attach it
+        var xsrfCookie: HTTPCookie? = nil
+        if let cookies: [HTTPCookie] = HTTPCookieStorage.shared.cookies {
+            for cookie in cookies {
+                if cookie.name == "XSRF-TOKEN" {
+                    xsrfCookie = cookie
+                }
+            }
+        }
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        
+        // Send the request to Udacity
+        NSLog("Attemping log out")
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            // Catch any communication errors
+            if (error != nil) {
+                NSLog("Log out failed for networking error: \(error)")
+                DispatchQueue.main.async() {
+                    completionHandler(Results.failedForNetworkingError)
+                }
+            }
+            
+            // Assumming at this point that the logout was sucessful; if it wasn't for whatever (unlikely) reason, the user will simply create a new session on the next login and the current one will expire on it's own
+            NSLog("Log out presumed successful")
+            DispatchQueue.main.async() {
+                completionHandler(Results.success)
+            }
+        }
+        task.resume()
     }
     
     func refreshStudentInformation(completionHandler: @escaping (Results) -> ()) {
