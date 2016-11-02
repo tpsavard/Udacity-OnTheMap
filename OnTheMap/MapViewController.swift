30950b1146e056e5a6fb59aea4ab427800479b2d
Timothy Savard <timsav@amazon.com>
2016-11-02 12:57:52 -0700

    Added map view and delegate.

---
+++ OnTheMap/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  OnTheMap
+//
+//  Created by Savard, Tim on 11/2/16.
+//  Copyright © 2016 Timothy Savard. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    
+
+}
