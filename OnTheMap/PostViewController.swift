b898e49bb60b524dec16b772234bb0165cdba68f
Timothy Savard <tpsavard@gmail.com>
2016-12-12 21:29:59 -0800

    Defined UI for post view.

--- OnTheMap/PostViewController.swift
+++ OnTheMap/PostViewController.swift
@@ -13,27 +13,39 @@ class PostViewController: UITableViewController {
     // MARK:- Table View Controller Methods
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section {
-        case 0:
-            return "1"
-        case 1:
-            return "2"
-        default:
-            return ""
-        }
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        switch section {
+        case 0,1:
+            return 1
+        case 2:
+            return 2
+        default:
+            return 0
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Determine the correct cell
+        var cellIdentifier: String = ""
+        switch (indexPath.section, indexPath.row) {
+        case (0,0):
+            cellIdentifier = "location"
+        case (0,1):
+            cellIdentifier = "map"
+        case (1,0):
+            cellIdentifier = "url"
+        case (2,0):
+            cellIdentifier = "done"
+        case (2,1):
+            cellIdentifier = "cancel"
+        default:
+            break
+        }
+        
         // Get a fresh cell
-        let cellIdentifier: String = "fieldCell"
         let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)!
         
         // TODO: Decorate & return the cell
