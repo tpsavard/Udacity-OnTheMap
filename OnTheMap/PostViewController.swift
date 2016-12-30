fe46688c284e04601daee06c1ceb8096dca1ff4c
Timothy Savard <timsav@amazon.com>
2016-12-29 16:24:09 -0800

    Implemented geocoding in post view.

--- OnTheMap/PostViewController.swift
+++ OnTheMap/PostViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class PostViewController: UITableViewController {
     
+    weak var locationField: UITextField!
+    weak var map: MKMapView!
     weak var urlField: UITextField!
     weak var doneButton: UIButton!
     
+    let geocoder: CLGeocoder = CLGeocoder()
     var locationFound: Bool = false
     var locationShown: Bool = false
 
@@ -62,13 +67,21 @@ class PostViewController: UITableViewController {
         
         // TODO: Decorate & return the cell
         switch cellIdentifier {
+        case "location":
+            if let view = cell.contentView.viewWithTag(1) as? UITextField {
+                locationField = view
+            }
+        case "map":
+            if let view = cell.contentView.viewWithTag(2) as? MKMapView {
+                map = view
+            }
         case "url":
-            if let urlView = cell.contentView.viewWithTag(2) as? UITextField {
-                urlField = urlView
+            if let view = cell.contentView.viewWithTag(3) as? UITextField {
+                urlField = view
             }
         case "done":
-            if let doneView = cell.contentView.viewWithTag(3) as? UIButton {
-                doneButton = doneView
+            if let view = cell.contentView.viewWithTag(4) as? UIButton {
+                doneButton = view
             }
         default:
             break
@@ -102,26 +115,72 @@ class PostViewController: UITableViewController {
         
         // For the location field, look up the location
         if sender.tag == 1 {
+            // Validation, before we update any UI
+            if locationField.text!.isEmpty {
+                self.showFailureAlert(
+                    title: NSLocalizedString("NoLocationFailureTitle", comment: "No Location failure alert title"),
+                    message: NSLocalizedString("NoLocationFailure", comment: "No Location failure text"))
+                return
+            }
+            
             // Look up the given location name
-            
-            // Add an annotation for the resolved location
-            
-            // Show the map cell
-            if !locationShown {
-                tableView.beginUpdates()
+            geocoder.geocodeAddressString(locationField.text!) {(placemarks: [CLPlacemark]?, error: Error?) in
+                // More validation, before we change pins
+                guard let placemarks = placemarks, placemarks.count > 0, error == nil else {
+                    self.showFailureAlert(
+                        title: NSLocalizedString("LocationNotFoundFailureTitle", comment: "Location not found failure alert title"),
+                        message: NSLocalizedString("LocationNotFoundFailure", comment: "Location not found failure text"))
+                    return
+                }
                 
-                locationShown = true
-                tableView.insertRows(at: [IndexPath(row: 1, section: 0)], with: .automatic)
+                // Show the map cell, if hasn't been shown already
+                if !self.locationShown {
+                    self.locationShown = true
+                    self.tableView.beginUpdates()
+                    self.tableView.insertRows(at: [IndexPath(row: 1, section: 0)], with: .automatic)
+                    self.tableView.endUpdates()
+                }
                 
-                tableView.endUpdates()
+                // Remove any prior annotations
+                self.map.removeAnnotations(self.map.annotations)
+                
+                // Add an annotation for the newly resolved location
+                let annotation: MKPointAnnotation = MKPointAnnotation()
+                annotation.coordinate = placemarks[0].location!.coordinate
+                
+                self.map.addAnnotation(annotation)
+                self.map.setCenter(annotation.coordinate, animated: true)
+                
+                self.locationFound = true
+                self.enableDone()
             }
         }
+    }
+    
+    @IBAction func done(sender: UIButton) {
+        cancel(sender: sender)
     }
     
     // MARK:- Other Methods
     
     func enableDone() {
         doneButton.isEnabled = !(urlField.text!.isEmpty) && locationFound
+    }
+    
+    func showFailureAlert(title: String, message: String) {
+        // Construct the alert ingredients
+        let alertController: UIAlertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle:UIAlertControllerStyle.alert)
+        let alertAction: UIAlertAction = UIAlertAction(
+            title: NSLocalizedString("OK", comment: "Not good-OK, not bad-OK, just OK-OK"),
+            style: UIAlertActionStyle.default,
+            handler: nil)
+        
+        // Showtime!
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
     }
 
 }
